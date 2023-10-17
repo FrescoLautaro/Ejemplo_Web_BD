4db0d5a6d3d0c2a1e2e29c2b1e8eb21d64b99872
FrancoMel123 <127970189+FrancoMel123@users.noreply.github.com>
2023-10-17 09:35:21 -0300

    Update ConectarBD.java

--- src/java/modelo/ConectarBD.java
+++ src/java/modelo/ConectarBD.java
@@ -4,10 +4,36 @@
  */
 package modelo;
 
-/**
- *
- * @author ET36
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConectarBD {
-    
+
+    private static Connection con;
+    private static String bd = "INSTITUTO";
+    private static String usuario = "root";
+    private static String passw = "";
+    private static String url = "jdbc:mysql://localhost/" + bd;
+
+    public static Connection abrir() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, usuario, passw);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Error: No se puedo cargar el controlador JDBC.");
+            throw new SQLException("Error en la conexion.", e);
+        }
+        return con;
+    }
+
+    public static void cerrar() {
+        try {
+            if (con != null) 
+                con.close();
+        } catch (SQLException e) {
+            System.out.println("Error: No se logro cerrar la conexion:\n" + e);
+        }
+    }
+
 }
