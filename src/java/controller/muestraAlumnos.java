661b082cd8e572fb6e91dbb2ba8c60fe8c7607c0
FrancoMel123 <127970189+FrancoMel123@users.noreply.github.com>
2023-10-17 09:33:53 -0300

    Update muestraAlumnos.java

--- src/java/controller/muestraAlumnos.java
+++ src/java/controller/muestraAlumnos.java
@@ -4,10 +4,41 @@
  */
 package controller;
 
-/**
- *
- * @author ET36
- */
-public class muestraAlumnos {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.Alumno;
+import modelo.GestorBD;
+
+@WebServlet(name = "muestraAlumnos", urlPatterns = {"/muestraAlumnos"})
+
+public class muestraAlumnos extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            ArrayList<Alumno> alumnos = new ArrayList();
+            GestorBD gestorBD = new GestorBD();
+            alumnos = gestorBD.leerTodos();
+            if (alumnos != null) {
+                request.setAttribute("alumnos", alumnos);
+                request.getRequestDispatcher("/listaAlumnos.jsp")
+                        .forward(request, response);
+            } else {
+                request.getRequestDispatcher("/noHayRegistros.jsp")
+                        .forward(request, response);
+            }
+
+        } finally {
+            out.close();
+        }
+    }
 }
