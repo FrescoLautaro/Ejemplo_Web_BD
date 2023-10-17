19fc9bd9fa9f5fd6976da4bb3b0a7234168d5be4
FrancoMel123 <127970189+FrancoMel123@users.noreply.github.com>
2023-10-17 09:34:23 -0300

    Update registro.java

--- src/java/controller/registro.java
+++ src/java/controller/registro.java
@@ -4,10 +4,41 @@
  */
 package controller;
 
-/**
- *
- * @author ET36
- */
-public class registro {
-    
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.*;
+
+@WebServlet(name = "registro", urlPatterns = {"/registro"})
+
+public class registro extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        try {
+            String nombre = request.getParameter("nombre");
+            int legajo = Integer.parseInt(request.getParameter("legajo"));
+            String apellido = request.getParameter("apellido");
+            Double promedio = Double.parseDouble(request.getParameter("promedio"));
+            GestorBD gestorBD = new GestorBD();
+            if (gestorBD.registrar(nombre, apellido, promedio)) {
+                Alumno al = new Alumno(legajo, nombre, apellido, promedio);
+                request.setAttribute("atribAlumn", al);
+                request.getRequestDispatcher("/muestraDatos.jsp")
+                        .forward(request, response);
+            } else {
+                request.getRequestDispatcher("/errorEnRegistro.jsp")
+                        .forward(request, response);
+            }
+        } finally {
+            out.close();
+        }
+    }
 }
